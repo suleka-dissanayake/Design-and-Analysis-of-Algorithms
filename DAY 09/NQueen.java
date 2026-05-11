fe5150c4d014297e0e11abee4936905698f546c1
Suleka Dissanayake <sulekadissanayake2003@gmail.com>
2026-05-11 12:26:27 +0530

    Files

--- DAY 09/NQueen.java
+++ DAY 09/NQueen.java
@@ -1,7 +1,7 @@
 class NQueen {
     static final int N = 4;
 
-    static void printBoard(int board[][]) {
+    static void printBoard(int[][] board) {
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 System.out.print(board[i][j] + " ");
@@ -10,18 +10,18 @@ class NQueen {
         }
     }
 
-    static boolean isSafe(int board[][], int row, int col) {
-        for (int i = 0; i < col; i++) {
+    static boolean isSafe(int[][] board, int row, int column) {
+        for (int i = 0; i < column; i++) {
             if (board[row][i] == 1)
                 return false;
         }
 
-        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+        for (int i = row, j = column; i >= 0 && j >= 0; i--, j--) {
             if (board[i][j] == 1)
                 return false;
         }
 
-        for (int i = row, j = col; i < N && j >= 0; i++, j--) {
+        for (int i = row, j = column; i < N && j >= 0; i++, j--) {
             if (board[i][j] == 1)
                 return false;
         }
@@ -29,19 +29,19 @@ class NQueen {
         return true;
     }
 
-    static boolean solveNQUtil(int board[][], int col) {
-        if (col >= N)
+    static boolean solve(int[][] board, int column) {
+        if (column >= N) {
             return true;
+        }
 
         for (int i = 0; i < N; i++) {
-            if (isSafe(board, i, col)) {
+            if (isSafe(board, i, column)) {
+                board[i][column] = 1;
 
-                board[i][col] = 1;
-
-                if (solveNQUtil(board, col + 1))
+                if (solve(board, column + 1))
                     return true;
 
-                board[i][col] = 0;
+                board[i][column] = 0;
             }
         }
 
@@ -49,10 +49,10 @@ class NQueen {
     }
 
     public static void main(String[] args) {
-        int board[][] = new int[N][N];
+        int[][] board = new int[N][N];
 
-        if (!solveNQUtil(board, 0)) {
-            System.out.println("Solution does not exist");
+        if (!solve(board, 0)) {
+            System.out.println("Solution does not exist!");
             return;
         }
 
