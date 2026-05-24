7c1feca1d89854e38f034e94a71421183d544e02
Suleka Dissanayake <sulekadissanayake2003@gmail.com>
2026-05-24 17:16:36 +0530

    Files

--- DAY 10/HamiltonianCycle.java
+++ DAY 10/HamiltonianCycle.java
@@ -1,8 +1,7 @@
 class HamiltonianCycle {
-    private static int V;
-    private static int[] path;
+    static int V = 5;
 
-    static boolean isSafe(int v, int[][] graph, int pos) {
+    static boolean isSafe(int v, int[][] graph, int[] path, int pos) {
         if (graph[path[pos - 1]][v] == 0)
             return false;
 
@@ -14,15 +13,15 @@ class HamiltonianCycle {
         return true;
     }
 
-    static boolean hamCycleUtil(int[][] graph, int pos) {
+    static boolean solve(int[][] graph, int[] path, int pos) {
         if (pos == V)
             return graph[path[pos - 1]][path[0]] == 1;
 
         for (int v = 1; v < V; v++) {
-            if (isSafe(v, graph, pos)) {
+            if (isSafe(v, graph, path, pos)) {
                 path[pos] = v;
 
-                if (hamCycleUtil(graph, pos + 1))
+                if (solve(graph, path, pos + 1))
                     return true;
 
                 path[pos] = -1;
@@ -32,35 +31,35 @@ class HamiltonianCycle {
         return false;
     }
 
-    static void solve(int[][] graph) {
-        V = graph.length;
-        path = new int[V];
+    static void hamiltonianCycle(int[][] graph) {
+        int[] path = new int[V];
 
         for (int i = 0; i < V; i++)
             path[i] = -1;
 
         path[0] = 0;
 
-        if (!hamCycleUtil(graph, 1)) {
-            System.out.println("Solution does not exist!");
+        if (!solve(graph, path, 1)) {
+            System.out.println("No cycle");
             return;
         }
 
-        System.out.println("Hamiltonian cycle exists:");
-        for (int i = 0; i < V; i++) {
-            System.out.print(path[i] + " -> ");
-        }
+        for (int i = 0; i < V; i++)
+            System.out.print(path[i] + " ");
+
         System.out.println(path[0]);
     }
 
     public static void main(String[] args) {
+
         int[][] graph = {
-            {0, 1, 0, 1},
-            {1, 0, 1, 1},
-            {0, 1, 0, 1},
-            {1, 1, 1, 0}
+                {0, 1, 0, 1, 0},
+                {1, 0, 1, 1, 1},
+                {0, 1, 0, 0, 1},
+                {1, 1, 0, 0, 1},
+                {0, 1, 1, 1, 0}
         };
 
-        solve(graph);
+        hamiltonianCycle(graph);
     }
 }
