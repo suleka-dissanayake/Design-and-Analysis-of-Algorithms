f1a21de7eec5e2f3a349c29a89e6ddd68ad24dd5
Suleka Dissanayake <sulekadissanayake2003@gmail.com>
2026-06-18 15:55:51 +0530

    Files

--- DAY 04/Factorial.java
+++ DAY 04/Factorial.java
@@ -1,8 +1,8 @@
 class Factorial{
     static int factorial(int num) {
-        if num == 0 || num == 1
+        if (num == 0 || num == 1)
             return 1;
-            
+
         return num * factorial(num - 1);
     }
 
